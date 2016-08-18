c680544bff35ef65fa1ba598cd04cd60ac63b621
stefo <stefomitev@gmail.com>
2016-08-19 01:56:50 +0200

    changing dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var package = require('./package.json');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var handlebars = require('gulp-compile-handlebars');
@@ -5,31 +6,33 @@ var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
  
 gulp.task('sass', function () {
-  return gulp.src('./src/style/*.scss')
+  return gulp.src('./src/styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./style'));
+    .pipe(gulp.dest('./dist/styles'));
 });
 
 gulp.task('sass:watch', ['sass'], function () {
-  return gulp.watch('./style/**/*.scss', ['sass']);
+  return gulp.watch('./styles/**/*.scss', ['sass']);
 });
 
 gulp.task('copy:fonts', function(){
-	return gulp.src('./src/style/fonts/*')
-	.pipe(gulp.dest('./style/fonts'));
+	return gulp.src('./src/styles/fonts/*')
+	.pipe(gulp.dest('./dist/styles/fonts'));
 });
 
 gulp.task('views', function () {
   var templateData = {
-      firstName: 'Kaanon'
+      version: package.version
   },
   options = {
       batch : ['./src/partials']
   }
 
-  return gulp.src('src/views/index.hbs')
+  return gulp.src('./src/views/index.hbs')
       .pipe(handlebars(templateData, options))
       .pipe(rename('index.html'))
       .pipe(htmlmin({collapseWhitespace: true}))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('default', ['sass', 'copy:fonts', 'views']);
