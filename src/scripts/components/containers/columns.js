f400e2957253afe8e3f2eeb3ff96b28dc2779523
stefanm <stefanm@backbase.com>
2016-10-15 22:21:06 -0500

    fixing columns issue, adding row wrapper

--- src/scripts/components/containers/columns.js
+++ src/scripts/components/containers/columns.js
@@ -2,9 +2,13 @@ import Container from './base';
 
 class Columns extends Container {
     render () {
-        return super.render(this.eachArea((children, index) => {
-            return <div key={'area_' + index} className={'col' + (this.props.single ? ' col--full': '')}>{children}</div>;
-        }));
+        return super.render(
+            <div className="row">
+                {this.eachArea((children, index) => {
+                    return <div key={'area_' + index} className={'col' + (this.props.single ? ' col--full': '')}>{children}</div>;
+                })}
+            </div>
+        );
     }
 }
 
